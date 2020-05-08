5b5f0b288b3b924ace47e31df9b4c934d3d16c08
LuckyMarty <62702759+LuckyMarty@users.noreply.github.com>
2020-05-08 19:40:21 +0200

    Embed Kick log

--- Commands/Admin/kick.js
+++ Commands/Admin/kick.js
@@ -13,5 +13,15 @@ module.exports = {
         const reason = (args.splice(1).join(" ") || 'Aucune raison spécifié');
 
         user ? message.guild.member(user).kick(reason) : message.channel.send("L'utilisateur n'existe pas !");
+        
+        const embed = new MessageEmbed()
+        .setAuthor(`${user.username} ${user.id}`)
+        .setColor("#dc143c")
+        .setDescription(`**Action** : kick\n **Raison** : ${reason}`)
+        .setThumbnail(user.avatarURL())
+        .setTimestamp()
+        .setFooter(message.author.username, message.author.avatarURL());
+
+        bot.channels.cache.get('708314429821026356').send(embed);
     }
 }
