25af325ffbdb4b094e864890cfa3c6f1b43725ae
LuckyMarty <62702759+LuckyMarty@users.noreply.github.com>
2020-05-09 17:19:38 +0200

    Bot Commands

---
+++ Commands/Bot/visiteur.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'visiteur',
+    aliases: ['visiteur'],
+    description: "Attribuer à tous les utilisateurs n'ayant pas de bagde, le badge visiteur",
+    category: "bot",
+    cooldown: 5,
+    usage: '',
+    permissions: true,
+    isUserAdmin: false,
+    
+    execute(bot, message, args) {
+        if (message)
+        {
+            function Attribution_Visiteur() 
+            {
+                let role = message.guild.roles.cache.find(r => r.id == '708402837998010379');
+                if (!role) return message.channel.send(`**${message.author.username}**, role not found`);
+            
+                message.guild.members.cache.filter(member => member.roles.cache.array().length <= 1).forEach(member => member.roles.add(role));
+                // console.log("visiteur");
+            }
+            setInterval(Attribution_Visiteur, 1000);
+        }
+    }
+}
