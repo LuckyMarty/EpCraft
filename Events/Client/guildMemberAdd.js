7e5303d7cbf48f441c1fd4fb7454f590b58b25dd
LuckyMarty <62702759+LuckyMarty@users.noreply.github.com>
2020-05-12 17:36:36 +0200

    Update guildMemberAdd.js

--- Events/Client/guildMemberAdd.js
+++ Events/Client/guildMemberAdd.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports = (Ecki, member) => {
-    const welcome = member.guild.channels.cache.find((channel) => channel.id === "709786898213896253");
+    const welcome = member.guild.channels.cache.find((channel) => channel.id === "430047753507176451");
     if (!welcome) return;
 
     return welcome.send(`Bienvenue à ${member.user} sur EpCraft !`);
