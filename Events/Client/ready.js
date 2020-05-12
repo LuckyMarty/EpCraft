0377830b793065a5d9ca01e929a9ab91a45d5c2f
LuckyMarty <62702759+LuckyMarty@users.noreply.github.com>
2020-05-12 17:31:14 +0200

    Update config

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const Ecki = new Discord.Client();
 const ping = require('minecraft-server-util');
-const config = require("../../config.json");
 
 module.exports = Ecki => {
     console.log('Connecté !');
@@ -11,7 +10,7 @@ module.exports = Ecki => {
     // Afficher le nombre de joueurs toutes les 1s
     function joueurs() {
         
-        ping(config.IP, 25565, (error, response) => {
+        ping(process.env.IP, 25565, (error, response) => {
             Ecki.user.setActivity("EpCraft " + response.onlinePlayers + "/" + response.maxPlayers +" (!help)", {type: "PLAYING"});
         });
     }
