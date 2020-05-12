d7334399a9e447ab2e841b985510488ff3ae433d
LuckyMarty <62702759+LuckyMarty@users.noreply.github.com>
2020-05-12 17:37:50 +0200

    Update guildMemberRemove.js

--- Events/Client/guildMemberRemove.js
+++ Events/Client/guildMemberRemove.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports = (Ecki, member) => {
-    const inLog = member.guild.channels.cache.find((channel) => channel.id === "708314429821026356");
+    const inLog = member.guild.channels.cache.find((channel) => channel.id === "708624241053925396");
     if (!inLog) return;
 
     const embed_Quite_User = new Discord.MessageEmbed()
