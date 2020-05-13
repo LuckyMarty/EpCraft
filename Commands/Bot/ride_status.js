21a91c039ed2d6074b46770bed692d7d13959278
LuckyMarty <62702759+LuckyMarty@users.noreply.github.com>
2020-05-13 20:58:46 +0200

    Update ride_status.js

--- Commands/Bot/ride_status.js
+++ Commands/Bot/ride_status.js
@@ -11,7 +11,6 @@ module.exports = {
     execute(Ecki, message, args) {
         const Discord = require('discord.js');
         const getJSON = require('get-json');
-        const config = require("../../config.json");
         const flags = require("../../json/drapeaux.json");
         const schedule = require('node-schedule');
 
@@ -67,7 +66,7 @@ module.exports = {
                 });
 
 
-                let channel = Ecki.channels.cache.get("710085144387518515");
+                let channel = Ecki.channels.cache.get("685837132673056778");
 
                 const status_ouvert = new Discord.MessageEmbed()
                 .setTitle("**Ride Status**")
